8cb782b810fd0b1643f10defe6e5d477bc39dd54
Laith <laith@programmer.net>
2023-12-07 09:27:29 +0300

    Initial Commit for Docs

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>DiscordRPC Docs</span>,
+  project: {
+    link: 'https://github.com/babyico/discordrpc',
+  },
+  chat: {
+    link: 'https://discord.com/invite/EXHhWpb5ps',
+  },
+  docsRepositoryBase: 'https://github.com/babyico/discordrpc',
+  footer: {
+    text: 'Discord RPC by Laith',
+  },
+}
+
+export default config
